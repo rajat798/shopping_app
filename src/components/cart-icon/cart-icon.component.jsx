be49f9fb44837fc7134f5490d3458195b7953278
rajat798 <rajatvarshney798@gmail.com>
2021-08-15 23:29:04 +0530

    updating app with selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 import {ReactComponent as ShoppngIcon} from '../../assets/shopping-bag.svg'
 import {connect} from 'react-redux'
 import {toggleCartHidden} from '../../redux/cart/cart.actions' 
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors'
 import './cart-icon.styles.scss'
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppngIcon className='shopping-icon'/>
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
 
@@ -17,4 +18,10 @@ const mapDispatchToProps = (dispatch) => {
         toggleCartHidden : () => dispatch(toggleCartHidden())
     }
 }
-export default connect(null,mapDispatchToProps)(CartIcon)
+
+
+const mapStateToProps = state => ({
+    itemCount : selectCartItemsCount(state)
+});
+
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon)
